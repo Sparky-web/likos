25ab6e1c9909217b91478461d685bb9afa780136
Владислав Бабинов <65344272+Sparky-web@users.noreply.github.com>
2023-04-14 21:41:40 +0500

    ss

--- frontend/src/templates/Layout.js
+++ frontend/src/templates/Layout.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
 
-import {createMuiTheme, ThemeProvider} from '@material-ui/core/styles';
+import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
 
 import "../css/General.css"
 import SEO from "./SEO";
@@ -27,13 +27,33 @@ function Layout(props) {
             <SEO />
             <ThemeProvider theme={theme}>
 
-                <header><Navbar/></header>
+                <header><Navbar /></header>
                 <main>
                     {props.children}
                 </main>
-                <footer><Footer/></footer>
+                <footer><Footer /></footer>
 
             </ThemeProvider>
+            <div
+                dangerouslySetInnerHTML={{
+                    __html: `
+    <script type="text/javascript">
+    (function(m,e,t,r,i,k,a){m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) };
+    m[i].l=1*new Date();
+    for (var j = 0; j < document.scripts.length; j++) {if (document.scripts[j].src === r) { return; }}
+k=e.createElement(t),a=e.getElementsByTagName(t)[0],k.async=1,k.src=r,a.parentNode.insertBefore(k,a)})
+    (window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+
+    ym(93225143, "init", {
+        clickmap:true,
+    trackLinks:true,
+    accurateTrackBounce:true
+});
+</script>
+<noscript><div><img src="https://mc.yandex.ru/watch/29761725" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
+    `
+                }}
+            />
         </React.StrictMode>
     );
 }
